3c9981762dab144b993d96b8b4aa96dae7046bf2
Nikita <nik.bagnuk@mail.ru>
2022-11-23 18:46:41 +0300

    Added gitignore

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+
+// task 1
+fun main(){
+    var arraylist = arrayListOf<Int>(1,2,3,4,5,7)
+    var min = arraylist.minOf { item -> item }
+    arraylist.replaceAll { arraylist.minOf { item -> item } }
+    min = 2
+    println(min)
+    var max = arraylist.maxOf { item -> item }
+    max = 10
+    println(max)
+
+    
+}
+
+
+
