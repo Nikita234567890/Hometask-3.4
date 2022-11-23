72ba9e62b9748381a6b10036f7002630b255c6ae
Nikita <nik.bagnuk@mail.ru>
2022-11-23 18:54:58 +0300

    Done task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 
 // task 1
-fun main(){
+fun main1(){
     var arraylist = arrayListOf<Int>(1,2,3,4,5,7)
     var min = arraylist.minOf { item -> item }
     arraylist.replaceAll { arraylist.minOf { item -> item } }
@@ -9,6 +9,15 @@ fun main(){
     var max = arraylist.maxOf { item -> item }
     max = 10
     println(max)
+}
+fun main(){
+    var arrnum = ArrayList<Int>()
+    arrnum.add(2)
+    arrnum.add(10)
+    arrnum.addAll(1, listOf(3,4,5,6))
+
+    var avr = arrnum.average()
+    println(avr)
 }
 
 
